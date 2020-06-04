af51b73e78ae04dca53c9c0f1f5ee89bf91bccfa
unknown <madis.tepp@gmail.com>
2020-06-04 19:11:10 +0200

    testing views.py

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -3,11 +3,11 @@ from .forms import ItemForm
 
 class TestItemForm(TestCase):
 
-    def test_item_name_is_required(self):
-        form = ItemForm({'name': ''})
-        self.assertFalse(form.is_valid())
-        self.assertIn('name', form.errors.keys())
-        self.assertEqual(form.errors['name'][0], u'This field is required.')
+    def test_item_name_is_required(self): #test that item name is mandatory
+        form = ItemForm({'name': ''}) #try to create item with empty string
+        self.assertFalse(form.is_valid()) #checks if  form is valid, which it is not due empty string
+        self.assertIn('name', form.errors.keys()) #making sure that error occured in the form
+        self.assertEqual(form.errors['name'][0], u'This field is required.') #checking that correct error message received
 
     
     def test_done_not_required(self):
@@ -17,4 +17,4 @@ class TestItemForm(TestCase):
 
     def test_fields_are_explicit_in_form_meta_class(self):
         form = ItemForm()
-        self.assertEqual(form.Meta.fields, ['name', 'done'])
+        self.assertEqual(form.Meta.fields, ['name', 'done']) #ensuring that only fields from models.py MetaClass are there
--- todo/test_views.py
+++ todo/test_views.py
@@ -1,6 +1,45 @@
 from django.test import TestCase
+from .models import Item
 
 # Create your tests here.
 
+class TestViews(TestCase):
 
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
   
+
+    def test_get_edit_item_page(self):  
+        item = Item.objects.create(name='create to do item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
+
+
+    def test_can_add_item(self):
+        response = self.client.post('/add', {'name': 'testing items'})
+        self.assertRedirects(response, '/')
+
+
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='create to do item') #create objects
+        response = self.client.get(f'/delete/{item.id}') #delete objects
+        self.assertRedirects(response, '/') #ensure correct redirect matches views.py redirect
+        existing_items = Item.objects.filter(id=item.id) #search for deleted items
+        self.assertEqual(len(existing_items), 0) #verify if length of items is 0 as all are deleted
+
+    def test_can_toggle_item(self):
+        item = Item.objects.create(name='create to do item', done=True) #create object with done status True
+        response = self.client.get(f'/toggle/{item.id}') #toggle function for created object
+        self.assertRedirects(response, '/') #ensure correct redirect matches views.py redirect
+        updated_item = Item.objects.get(id=item.id) #get the updated item that is now done=False
+        self.assertFalse(updated_item.done) #ensure that false that item is done , check previous
